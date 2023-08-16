40e3692c9847ca8f52c177005053f71617e57783
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-08-16 23:24:59 +0530

    Add explore page mapping for home page

---
+++ src/main/java/com/codemind/PlayCenter/controller/ExploreController.java
@@ -0,0 +1,15 @@
+package com.codemind.PlayCenter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/explore")
+public class ExploreController {
+
+    @GetMapping("/explorePage")
+    private String showExplorePage(){
+        return "/homeDirectory/explore-page";
+    }
+}
