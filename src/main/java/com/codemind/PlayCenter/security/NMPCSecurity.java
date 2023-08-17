cd51e4e80e2360b5e3ed30e7f1a975303b0c4816
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-08-17 15:58:36 +0530

    Security configuration class is created

---
+++ src/main/java/com/codemind/PlayCenter/security/NMPCSecurity.java
@@ -0,0 +1,37 @@
+package com.codemind.PlayCenter.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class NMPCSecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity.authorizeHttpRequests(configurer->
+
+                        configurer
+                                .requestMatchers("/").permitAll()
+                                .requestMatchers("/loginPage").permitAll()
+                                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                                .requestMatchers("/systems/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+
+                )
+                .formLogin(form->
+
+                        form
+                                .loginProcessingUrl("/authenticateUser")
+                                .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll
+                );
+
+        return httpSecurity.build();
+    }
+
+}
