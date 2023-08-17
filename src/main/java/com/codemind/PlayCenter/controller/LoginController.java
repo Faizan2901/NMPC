958fb54fbde25337a6d72da8a20e4d8375a57efa
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-08-17 15:59:01 +0530

    Login controller is created

---
+++ src/main/java/com/codemind/PlayCenter/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.codemind.PlayCenter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/loginPage")
+    private String getLoginPage(){
+        return "/homeDirectory/login/login-page";
+    }
+}
