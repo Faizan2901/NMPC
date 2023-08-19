63897e11c2cb7b4dde8ca5050f197de1ae0fd4fc
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-08-19 15:31:51 +0530

    NMPCSecurity default is implemented for authentication

--- src/main/java/com/codemind/PlayCenter/security/NMPCSecurity.java
+++ src/main/java/com/codemind/PlayCenter/security/NMPCSecurity.java
@@ -1,14 +1,32 @@
 package com.codemind.PlayCenter.security;
 
+import com.codemind.PlayCenter.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class NMPCSecurity {
 
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(UserService userService){
+
+        DaoAuthenticationProvider auth=new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService);
+        auth.setPasswordEncoder(bCryptPasswordEncoder());
+        return auth;
+
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
 
