3f7f13ddc309077bd009abfcad90d884f5d1f6df
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-08-19 22:00:52 +0530

    CustomAuthenticationSuccessHandler is implemented for redirection of after successfully login of student

---
+++ src/main/java/com/codemind/PlayCenter/security/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,23 @@
+package com.codemind.PlayCenter.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        // Customize the redirection URL based on your requirements
+        response.sendRedirect("/dashboard"); //// Redirect to the "/dashboard" page, for example
+
+    }
+}
