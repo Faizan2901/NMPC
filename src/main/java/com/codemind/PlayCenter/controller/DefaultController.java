1b84d13b6ca8faa692980bf483573ec632105a66
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-08-20 20:47:25 +0530

    Get mapping for home page is renamed to "nmpc-home"

--- src/main/java/com/codemind/PlayCenter/controller/DefaultController.java
+++ src/main/java/com/codemind/PlayCenter/controller/DefaultController.java
@@ -2,11 +2,12 @@ package com.codemind.PlayCenter.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DefaultController {
 
-    @GetMapping("/")
+    @GetMapping("/nmpc-home")
     private String showHomePage(){
         return "/homeDirectory/home-Page";
     }
