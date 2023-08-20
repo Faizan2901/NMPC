b33734386b0ce92960e94bc23da7b33f9a863279
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-08-20 21:23:03 +0530

    Optimize import

--- src/main/java/com/codemind/PlayCenter/security/NMPCSecurity.java
+++ src/main/java/com/codemind/PlayCenter/security/NMPCSecurity.java
@@ -5,11 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 public class NMPCSecurity{
