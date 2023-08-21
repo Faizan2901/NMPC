c58e8fa8cd2aea04181b164377cf5db44136e092
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-08-21 13:51:54 +0530

    SQL Tables scripts is added in project file structure

---
+++ sql-scripts/nmpc-sql-tables-scripts.sql
@@ -0,0 +1,102 @@
+USE `nmpc`;
+
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS `student`;
+DROP TABLE IF EXISTS `role`;
+SET foreign_key_checks = 1;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` char(80) NOT NULL,
+  `enabled` tinyint NOT NULL,  
+  `first_name` varchar(64) NOT NULL,
+  `last_name` varchar(64) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+-- NOTE: The passwords are encrypted using BCrypt
+--
+-- A generation tool is avail at: http://www.luv2code.com/generate-bcrypt-password
+--
+-- Default passwords here are: fun123
+--
+
+INSERT INTO `student` (`username`,`password`,`enabled`, `first_name`, `last_name`, `email`)
+VALUES 
+('hassan','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1,'Hassan', 'Mutarwala', 'hassan@codemind.com'),
+('abdul','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1,'Abdul Aziz', 'Jangiwala', 'abdulaziz@codemind.com'),
+('faizan','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1,'Mohammad Faizan', 'Sopariwala', 'faizan@codemind.com');
+
+
+
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `role` (name)
+VALUES 
+('ROLE_STUDENT'),('ROLE_TEACHER'),('ROLE_ADMIN');
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+--
+-- Table structure for table `users_roles`
+--
+
+DROP TABLE IF EXISTS `students_roles`;
+
+CREATE TABLE `students_roles` (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  
+  PRIMARY KEY (`user_id`,`role_id`),
+  
+  KEY `FK_ROLE_idx` (`role_id`),
+  
+  CONSTRAINT `FK_USER_05` FOREIGN KEY (`user_id`) 
+  REFERENCES `user` (`id`) 
+  ON DELETE NO ACTION ON UPDATE NO ACTION,
+  
+  CONSTRAINT `FK_ROLE` FOREIGN KEY (`role_id`) 
+  REFERENCES `role` (`id`) 
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+--
+-- Dumping data for table `users_roles`
+--
+
+INSERT INTO `students_roles` (user_id,role_id)
+VALUES 
+(1, 1),
+(2, 1),
+(2, 2),
+(3, 1),
+(3, 2),
+(3, 3)
