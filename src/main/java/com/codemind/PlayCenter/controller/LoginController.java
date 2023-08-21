189ac2e252de76e75e2af7625fdee8f841e325a5
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-08-21 16:12:51 +0530

    Logout session is handled properly.

--- src/main/java/com/codemind/PlayCenter/controller/LoginController.java
+++ src/main/java/com/codemind/PlayCenter/controller/LoginController.java
@@ -1,10 +1,17 @@
 package com.codemind.PlayCenter.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
+
+    SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
 
     @GetMapping("/loginPage")
     private String getLoginPage(){
@@ -14,5 +21,12 @@ public class LoginController {
     @GetMapping("/dashboard")
     private String getDashboardPage(){
         return "/homeDirectory/login/dash-board";
+    }
+
+    @PostMapping("/logout")
+    public String performLogout(Authentication authentication, HttpServletRequest request, HttpServletResponse response) {
+        // .. perform logout
+        this.logoutHandler.logout(request, response, authentication);
+        return "redirect:/nmpc-home";
     }
 }
