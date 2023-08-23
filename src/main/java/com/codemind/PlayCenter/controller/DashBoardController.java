e4d206bb053f8de47bcd46c0d21eb094cace7b2d
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-08-23 16:43:27 +0530

    Post mapping for getting all check student in backend for DB storage purpose.

--- src/main/java/com/codemind/PlayCenter/controller/DashBoardController.java
+++ src/main/java/com/codemind/PlayCenter/controller/DashBoardController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -65,6 +67,14 @@ public class DashBoardController {
         model.addAttribute("allStudents",students);
 
         return "/homeDirectory/attendance-page";
+    }
+
+    @PostMapping("/fill-info")
+    private String showAttendedStudent(@RequestParam("selectedItems") List<String> selectedItems){
+        for(String seleString:selectedItems){
+            System.out.println(seleString);
+        }
+        return "";
     }
 
 }
