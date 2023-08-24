dc282b37da2b5a54fcf031cfc9165a7f11fdf264
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-08-24 16:16:20 +0530

    Implement DAO for storing attendance of student.

---
+++ src/main/java/com/codemind/PlayCenter/dao/StudentAttendanceDAO.java
@@ -0,0 +1,18 @@
+package com.codemind.PlayCenter.dao;
+
+
+import com.codemind.PlayCenter.entity.StudentAttendance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Locale;
+
+public interface StudentAttendanceDAO extends JpaRepository<StudentAttendance,Integer> {
+
+
+    List<StudentAttendance> findByStudentUsername(String userName);
+
+    StudentAttendance findByStudentUsernameAndDate(String userName,LocalDate date);
+
+}
