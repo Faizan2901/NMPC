38cdee4e1e5c1442e8541e5494d0b52f455956f3
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-09-05 22:34:03 +0530

    Mapping method is updated according to admission date of student.

--- src/main/java/com/codemind/PlayCenter/controller/StudentController.java
+++ src/main/java/com/codemind/PlayCenter/controller/StudentController.java
@@ -8,6 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/student")
 public class StudentController {
@@ -23,8 +28,19 @@ public class StudentController {
         String authenticateUserName=authController.getAuthenticateUserName();
         Student student=studentDAO.findByUserName(authenticateUserName);
 
-        model.addAttribute("username",student.getFirstName());
+        LocalDate admissionDate=student.getAdmissionDate();
+        LocalDate date1 =LocalDate.of(2011,12,12);
+        LocalDate date2 =LocalDate.now();
+        List<String> attendanceMonth=new ArrayList<>();
+        while(admissionDate.isBefore(date2)){
+            System.out.println(admissionDate.toString());
+            System.out.println(admissionDate.getMonth()+"-"+admissionDate.getYear());
+            attendanceMonth.add(admissionDate.getMonth()+"-"+admissionDate.getYear());
+            admissionDate = admissionDate.plus(Period.ofMonths(1));
+        }
 
+        model.addAttribute("username",student.getFirstName());
+        model.addAttribute("attendanceMonth",attendanceMonth);
         return "/homeDirectory/student-dashboard";
     }
 
