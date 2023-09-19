e9c19313d39239826024d33b86e12768e33612b2
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-09-19 22:35:35 +0530

    Query is added for getting count of attendance of given month and by student name.

--- src/main/java/com/codemind/PlayCenter/dao/StudentAttendanceDAO.java
+++ src/main/java/com/codemind/PlayCenter/dao/StudentAttendanceDAO.java
@@ -3,6 +3,8 @@ package com.codemind.PlayCenter.dao;
 
 import com.codemind.PlayCenter.entity.StudentAttendance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -16,5 +18,7 @@ public interface StudentAttendanceDAO extends JpaRepository<StudentAttendance,In
 
     StudentAttendance findByStudentUsernameAndDate(String userName,LocalDate date);
 
+    @Query(value = "SELECT COUNT(*) FROM StudentAttendance s WHERE s.studentUsername = :userName AND s.date LIKE %:dateMonth%")
+    int findAttendanceByStudentNameAndMonth(@Param("userName") String userName,@Param("dateMonth") String dateMonth);
 
 }
